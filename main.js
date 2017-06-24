fbe5266251cd4cf05e14828c4ef0c8f0372f9a86
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-06-24 15:28:31 -0400

    Add React component directly in HTML. Ugly, but rendering to the page

--- main.js
+++ main.js
@@ -18,26 +18,26 @@ $('#addIt').click(function addItem(){
   //grab value of inputs
   var priceInput = $('#price').val();
   var itemInput = $('#item').val();
-  var newItem = $("<li></li>", {id : priceInput})
+  //create new elements with id
+  var newItem = $("<li></li>", {id : priceInput});
+  var totalSpot = $('#total');
   newItem.append( itemInput + " " + priceInput);
   var removeBtn = $('<button></button').append('-');
+  var total = 0;
   //push value to the array
   pricesArray.push(priceInput);
-  // console.log(priceInput);
-  // console.log(pricesArray);
-  var total = 0;
 
   //loop it through to add up the prices
 for (var i = 0; i < pricesArray.length; i++) {
     total += pricesArray[i] << 0;
-
-  console.log(total);
-  console.log(pricesArray);
 }
 
   //put items on the page
   $('ul').append(newItem);
   $(newItem).append(removeBtn);
+  $(totalSpot).textContent = total;
+  console.log(total);
+
 });
 
 //remove items from DOM and array -- successfully removing from DOM. cannot grab value of removed li to either remove from array or subtract from array sum
@@ -46,13 +46,16 @@ for (var i = 0; i < pricesArray.length; i++) {
     var itemId = $(this).attr('id');
     console.log('remove button clicked');
     $(this).closest('li').remove();
+
     //re-add the items in the array
     for (var i = 0; i < pricesArray.length; i++) {
         total += pricesArray[i] << 0;
-  }
+      }
+
+  //subtract value from array total
       itemIdPrice = parseInt(itemId, 10);
       newTotal = total - itemIdPrice;
-      // console.log(arrayTotal);
       console.log(newTotal);
+
 
 });
